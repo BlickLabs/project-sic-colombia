96fa7c1542f48d1dd510ef631bc69817e3efc181
Raúl Marty <santor3medio@gmail.com>
2018-06-28 12:12:32 -0500

    #5. Changes in iframe-maps.js

--- src/js/iframe-maps.js
+++ src/js/iframe-maps.js
@@ -16,15 +16,15 @@ $(function(){
   });
 
   $('#changeBogota').click(function () {
-    $('#iframeLocations').attr('src', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14382.935083291552!2d-100.3428762!3d25.6801186!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x85955ecaad366e8a!2sSic+Comercializaci%C3%B3n+Y+Servicios!5e0!3m2!1ses-419!2smx!4v1527528793920');
+    $('#iframeLocations').attr('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.584938552632!2d-74.05992098523778!3d4.667846896609352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9af5bfee325b%3A0xe7bda34e87ca683b!2sCra.+16a+%2380-79%2C+Bogot%C3%A1%2C+Colombia!5e0!3m2!1ses-419!2smx!4v1528730961578');
   });
 
   $('#changeMedellin').click(function () {
-    $('#iframeLocations').attr('src', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14382.935083291552!2d-100.3428762!3d25.6801186!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x85955ecaad366e8a!2sSic+Comercializaci%C3%B3n+Y+Servicios!5e0!3m2!1ses-419!2smx!4v1527528793920');
+    $('#iframeLocations').attr('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.40316057219!2d-75.57219848523091!3d6.210438395504208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e44282bbf4d4c25%3A0x5f65bca9083f1efb!2sCra.+43+%2310-2%2C+Medell%C3%ADn%2C+Antioquia%2C+Colombia!5e0!3m2!1ses-419!2smx!4v1528731654201');
   });
 
   $('#changeSantiago').click(function () {
-    $('#iframeLocations').attr('src', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14382.935083291552!2d-100.3428762!3d25.6801186!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x85955ecaad366e8a!2sSic+Comercializaci%C3%B3n+Y+Servicios!5e0!3m2!1ses-419!2smx!4v1527528793920');
+    $('#iframeLocations').attr('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.2083490823106!2d-70.63159388480086!3d-33.44387798077579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c578eb440f4d%3A0xcec2af30369b8d9!2sSeminario+263%2C+Providencia%2C+Regi%C3%B3n+Metropolitana%2C+Chile!5e0!3m2!1ses-419!2smx!4v1528731413223');
   });
 
   $('#changeBuenosAires').click(function () {
