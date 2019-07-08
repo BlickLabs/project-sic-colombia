dd43affb661f275ef8d9215c20795a2a51a69363
Raúl Marty <santor3medio@gmail.com>
2019-07-08 06:57:13 -0500

    #15. Add for the spanish and english version of contact forms the new method of integration.

--- src/js/contact-form.js
+++ src/js/contact-form.js
@@ -1,57 +1,67 @@
+//////////////////////////////////////////
+// Formulario de contacto en español
+//////////////////////////////////////////
+
 $('form[name="sic-contact-form"]').validate({
   name: 'required',
   email: 'required',
+  message: 'required',
   messages: {
-   name: "Por favor, introduce tu nombre",
-   email: "Por favor introduce tu correo electrónico",
+    name: "Por favor, introduce tu nombre",
+    email: "Por favor introduce tu correo electrónico",
+    message: "Por favor, introduce tu mensaje"
   },
   submitHandler: function(form) {
-   var data = $('form[name="sic-contact-form"]').serialize();
-   console.log(data);
-   $.ajax({
-     url: 'http://integrations.blick.mx/sic-co/contact/form/',
-     method: 'POST',
-     data: data
-   }).done(function(data) {
-     if (parseInt(data) === 1) {
-       alertify.logPosition("bottom right");
-       alertify.success("Se ha enviado el email. Gracias por contactarnos!.");
-       $('form[name="sic-contact-form"]')[0].reset();
-     }
-   });
+    var formData = new FormData(document.getElementById('sic-contact-form'));
+
+    $.ajax({
+      type: 'POST',
+      url: 'https://sicco.com.co/integracion-formularios/contacto-spanol.php',
+      data: formData,
+      processData: false,
+      contentType: false,
+      cache: false
+    }).done(function() {
+      document.getElementById('sic-contact-form').reset();
+      alertify.success('Se ha enviado el email. Gracias por contactarnos!');
+    });
   },
   invalidHandler: function(event, validator) {
-   var errors = validator.numberOfInvalids();
-   alertify.logPosition("bottom right");
-   alertify.error("Verifica la información. Tienes " + errors + " errores.");
+    var errors = validator.numberOfInvalids();
+    alertify.error("Verifica la información. Tienes " + errors + " errores.");
   }
 });
+
+//////////////////////////////////////////
+// Formulario de contacto en inglés
+//////////////////////////////////////////
 
 $('form[name="sic-contact-form-en"]').validate({
   name: 'required',
   email: 'required',
+  message: 'required',
   messages: {
-   name: "Please, enter your name",
-   email: "Please, enter your email",
+    name: "Please, enter your name",
+    email: "Please, enter your email",
+    message: "Please, enter your message",
   },
   submitHandler: function(form) {
-   var data = $('form[name="sic-contact-form-en"]').serialize();
-   console.log(data);
-   $.ajax({
-     url: 'http://integrations.blick.mx/sic-co/contact/form/',
-     method: 'POST',
-     data: data
-   }).done(function(data) {
-     if (parseInt(data) === 1) {
-       alertify.logPosition("bottom right");
-       alertify.success("The email has been sent. Thanks for contact us!");
-       $('form[name="sic-contact-form-en"]')[0].reset();
-     }
-   });
+    var formData = new FormData(document.getElementById('sic-contact-form-en'));
+
+    $.ajax({
+      type: 'POST',
+      url: 'https://sicco.com.co/integracion-formularios/contacto-ingles.php',
+      data: formData,
+      processData: false,
+      contentType: false,
+      cache: false
+    }).done(function() {
+      document.getElementById('sic-contact-form-en').reset();
+      alertify.success('The email has been sent. Thanks for contact us!');
+    });
   },
   invalidHandler: function(event, validator) {
-   var errors = validator.numberOfInvalids();
-   alertify.logPosition("bottom right");
-   alertify.error("Verify yout information. You have " + errors + " errors.");
+    var errors = validator.numberOfInvalids();
+    alertify.error("Verify yout information. You have " + errors + " errors.");
   }
 });
